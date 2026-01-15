d168630416ce4bd4f4b9a978c09186981a87f0ad
imac-nugroho <nugrohowijayanto@nugrohos-iMac.local>
2026-01-15 08:33:32 +0700

    Drop user table

---
+++ src/migrations/20260115013139-drop-users-table.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users');
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      nip: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      department: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  }
+};
